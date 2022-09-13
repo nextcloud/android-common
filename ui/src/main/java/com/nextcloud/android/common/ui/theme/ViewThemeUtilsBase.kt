5c533ef7112f158be4bebb66740253eb42eae948
Álvaro Brey <alvaro.brey@nextcloud.com>
2022-09-13 16:11:37 +0200

    ViewThemeUtilsBase: schemes should be private
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

--- ui/src/main/java/com/nextcloud/android/common/ui/theme/ViewThemeUtilsBase.kt
+++ ui/src/main/java/com/nextcloud/android/common/ui/theme/ViewThemeUtilsBase.kt
@@ -28,7 +28,7 @@ import android.view.View
 import com.nextcloud.android.common.ui.util.PlatformThemeUtil
 import scheme.Scheme
 
-open class ViewThemeUtilsBase(val schemes: MaterialSchemes) {
+open class ViewThemeUtilsBase(private val schemes: MaterialSchemes) {
     /**
      * Scheme for painting elements
      */
