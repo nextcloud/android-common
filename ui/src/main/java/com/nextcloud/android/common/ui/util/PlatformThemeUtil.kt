071ec0ba2fa62e502ea094d1d93076cdcb32c163
Álvaro Brey <alvaro.brey@nextcloud.com>
2022-10-04 13:29:35 +0200

    PlatformThemeUtil: make isDarkMode accessible from Java
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

--- ui/src/main/java/com/nextcloud/android/common/ui/util/PlatformThemeUtil.kt
+++ ui/src/main/java/com/nextcloud/android/common/ui/util/PlatformThemeUtil.kt
@@ -25,6 +25,7 @@ import android.content.Context
 import android.content.res.Configuration
 
 object PlatformThemeUtil {
+    @JvmStatic
     fun isDarkMode(context: Context): Boolean =
         when (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
             Configuration.UI_MODE_NIGHT_YES -> true
