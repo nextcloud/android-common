51a88f51f38518ab452ff5330b1fbb9922e2ecee
alperozturk <alper_ozturk@proton.me>
2024-03-04 13:26:31 +0100

    Use Common-UI Lib
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- ui/src/main/java/com/nextcloud/android/common/ui/util/extensions/SchemeExtensions.kt
+++ ui/src/main/java/com/nextcloud/android/common/ui/util/extensions/SchemeExtensions.kt
@@ -2,7 +2,6 @@
  * Nextcloud Android Common Library
  *
  * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
- * SPDX-FileCopyrightText: 2024 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-License-Identifier: MIT
  */
 
