005d75cbf77d0d79059a4bcf5078f63bd1fdc0bc
Andy Scherzinger <info@andy-scherzinger.de>
2024-04-18 16:03:08 +0200

    Fix SPDX headers
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- material-color-utilities/src/main/java/dynamiccolor/Variant.java
+++ material-color-utilities/src/main/java/dynamiccolor/Variant.java
@@ -1,4 +1,8 @@
 /*
+ * SPDX-FileCopyrightText: 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+/*
  * Copyright 2022 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
