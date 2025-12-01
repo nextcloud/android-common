0e40da71e85c46980eb80d5358358bc0ec7b7791
Andy Scherzinger <info@andy-scherzinger.de>
2025-12-01 11:32:05 +0100

    chore: Automated Code Change sync 19/11/2025
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- material-color-utilities/src/main/java/dynamiccolor/ColorSpec2025.java
+++ material-color-utilities/src/main/java/dynamiccolor/ColorSpec2025.java
@@ -428,7 +428,8 @@ final class ColorSpec2025 extends ColorSpec2021 {
                     return surfaceContainerHigh();
                   }
                 })
-            .setContrastCurve((s) -> s.isDark ? getContrastCurve(11) : getContrastCurve(9))
+            .setContrastCurve(
+                (s) -> s.isDark && s.platform == PHONE ? getContrastCurve(11) : getContrastCurve(9))
             .build();
     return super.onSurface().toBuilder()
         .extendSpecVersion(SpecVersion.SPEC_2025, color2025)
@@ -625,15 +626,23 @@ final class ColorSpec2025 extends ColorSpec2021 {
                       return tMaxC(s.primaryPalette, 0, 90);
                     }
                   } else if (s.variant == EXPRESSIVE) {
-                    return tMaxC(
-                        s.primaryPalette,
-                        0,
-                        Hct.isYellow(s.primaryPalette.getHue())
-                            ? 25
-                            : Hct.isCyan(s.primaryPalette.getHue()) ? 88 : 98);
+                    if (s.platform == PHONE) {
+                      return tMaxC(
+                          s.primaryPalette,
+                          0,
+                          Hct.isYellow(s.primaryPalette.getHue())
+                              ? 25
+                              : Hct.isCyan(s.primaryPalette.getHue()) ? 88 : 98);
+                    } else { // WATCH
+                      return tMaxC(s.primaryPalette);
+                    }
                   } else { // VIBRANT
-                    return tMaxC(
-                        s.primaryPalette, 0, Hct.isCyan(s.primaryPalette.getHue()) ? 88 : 98);
+                    if (s.platform == PHONE) {
+                      return tMaxC(
+                          s.primaryPalette, 0, Hct.isCyan(s.primaryPalette.getHue()) ? 88 : 98);
+                    } else { // WATCH
+                      return tMaxC(s.primaryPalette);
+                    }
                   }
                 })
             .setIsBackground(true)
@@ -1529,7 +1538,7 @@ final class ColorSpec2025 extends ColorSpec2021 {
 
   private static ContrastCurve getContrastCurve(double defaultContrast) {
     if (defaultContrast == 1.5) {
-      return new ContrastCurve(1.5, 1.5, 3, 4.5);
+      return new ContrastCurve(1.5, 1.5, 3, 5.5);
     } else if (defaultContrast == 3) {
       return new ContrastCurve(3, 3, 4.5, 7);
     } else if (defaultContrast == 4.5) {
