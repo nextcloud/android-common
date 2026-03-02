da67e59277d725cb78d3767efb4b520e8a6fc51b
sowjanyakch <sowjanya.kch@gmail.com>
2026-03-02 13:53:45 +0100

    add new cloud id
    Signed-off-by: sowjanyakch <sowjanya.kch@gmail.com>

--- core/src/main/java/com/nextcloud/android/common/core/utils/ecosystem/EcosystemManager.kt
+++ core/src/main/java/com/nextcloud/android/common/core/utils/ecosystem/EcosystemManager.kt
@@ -40,11 +40,10 @@ class EcosystemManager(
         private const val EXTRA_KEY_ACCOUNT = "KEY_ACCOUNT"
 
         const val ACCOUNT_NAME_PATTERN_REGEX =
-            "^[A-Za-z0-9._%+-]+(?:" +
-                "@(?:[A-Za-z0-9-]+\\.)+[A-Za-z]{2,}(?:@(?:[A-Za-z0-9-]+\\.)+[A-Za-z]{2,})?" +
-                "|" +
-                "@(?:\\d{1,3}\\.){2,3}\\d{1,3}(?::\\d{1,5})?" +
-                ")$"
+            "^[A-Za-z0-9._%+-]+(?:@(?:[A-Za-z0-9-]+\\.)+[A-Za-z]{2,})?" +
+                "@(?:(?:[A-Za-z0-9-]+\\.)+[A-Za-z]{2,}|(?:\\d{1,3}\\.){3}\\d{1,3})" +
+                "(?::\\d{1,5})?" +
+                "(?:/[^\\s?#]*)?$"
     }
 
     private val accountNamePattern = Pattern.compile(ACCOUNT_NAME_PATTERN_REGEX)
