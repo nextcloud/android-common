9ec5ece48e55cdb6071e1e90b368bea8e7a30717
alperozturk96 <alper_ozturk@proton.me>
2026-04-20 16:00:01 +0200

    feat: unified share
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

---
+++ ui/src/main/java/com/nextcloud/android/common/ui/share/repository/UnifiedShareRemoteRepository.kt
@@ -0,0 +1,16 @@
+/*
+ * Nextcloud Android Common Library
+ *
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.nextcloud.android.common.ui.share.repository
+
+import com.nextcloud.android.common.ui.share.model.UnifiedShares
+
+class UnifiedShareRemoteRepository: UnifiedShareRepository {
+    override suspend fun fetchShares(): List<UnifiedShares> {
+        TODO("Not yet implemented")
+    }
+}
