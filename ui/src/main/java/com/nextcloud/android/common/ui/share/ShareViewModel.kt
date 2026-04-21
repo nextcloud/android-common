2e7aa9e04c7c81fcaf52acdceb57eb119ba9f95a
alperozturk96 <alper_ozturk@proton.me>
2026-04-21 10:08:58 +0200

    adopt mock models to api models
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- ui/src/main/java/com/nextcloud/android/common/ui/share/ShareViewModel.kt
+++ ui/src/main/java/com/nextcloud/android/common/ui/share/ShareViewModel.kt
@@ -9,7 +9,8 @@ package com.nextcloud.android.common.ui.share
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.nextcloud.android.common.ui.share.model.ui.UnifiedShares
+import com.nextcloud.android.common.ui.network.ApiResult
+import com.nextcloud.android.common.ui.share.model.ui.UnifiedShare
 import com.nextcloud.android.common.ui.share.repository.ShareRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,16 +18,39 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class ShareViewModel(private val repository: ShareRepository): ViewModel() {
-    private val _shares = MutableStateFlow<List<UnifiedShares>>(emptyList())
-    val shares: StateFlow<List<UnifiedShares>> = _shares
+class ShareViewModel(
+    private val repository: ShareRepository
+) : ViewModel() {
+
+    private val _shares = MutableStateFlow<List<UnifiedShare>>(emptyList())
+    val shares: StateFlow<List<UnifiedShare>> = _shares
+
+    private val _loading = MutableStateFlow(false)
+    val loading: StateFlow<Boolean> = _loading
+
+    private val _error = MutableStateFlow<String?>(null)
+    val error: StateFlow<String?> = _error
 
     init {
+        loadShares()
+    }
+
+    private fun loadShares() {
         viewModelScope.launch(Dispatchers.IO) {
-            val shares = repository.fetchShares()
-            _shares.update {
-                shares
+            _loading.value = true
+            _error.value = null
+
+            when (val result = repository.fetchShares()) {
+                is ApiResult.Success -> {
+                    _shares.update { result.data }
+                }
+
+                is ApiResult.Error -> {
+                    _error.value = result.error.ocs.meta.message
+                }
             }
+
+            _loading.value = false
         }
     }
 }
