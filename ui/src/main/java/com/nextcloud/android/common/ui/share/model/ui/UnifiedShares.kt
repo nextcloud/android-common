1e0bb79c9fb8f153a7478325701600ced4828590
alperozturk96 <alper_ozturk@proton.me>
2026-04-21 12:41:47 +0200

    add translations, bind ui actions
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- ui/src/main/java/com/nextcloud/android/common/ui/share/model/ui/UnifiedShares.kt
+++ ui/src/main/java/com/nextcloud/android/common/ui/share/model/ui/UnifiedShares.kt
@@ -11,19 +11,39 @@ import com.nextcloud.android.common.ui.share.model.api.owner.Owner
 import com.nextcloud.android.common.ui.share.model.api.user.ShareUser
 
 data class UnifiedShare(
-    val id: String,
+    val id: String?,
     val sources: List<ShareUser>,
     val recipients: List<ShareUser>,
     val properties: Map<String, Map<String, List<String>>>,
 
     val lastUpdated: Long,
-    val owner: Owner,
+    val owner: Owner?,
 
-    val permission: UnifiedSharePermission,
-    val type: UnifiedShareType,
+    val permission: UnifiedSharePermission?,
+    val type: UnifiedShareType?,
     val category: UnifiedShareCategory,
     val label: String,
     val note: String = "",
     val password: String = "",
     val limit: UnifiedShareDownloadLimit? = null
-)
+) {
+    companion object {
+        fun new(): UnifiedShare {
+            return UnifiedShare(
+                id = null,
+                sources = listOf(),
+                recipients = listOf(),
+                properties = mapOf(),
+                lastUpdated = -1,
+                owner = null,
+                permission = null,
+                type = null,
+                category = UnifiedShareCategory.Invited,
+                label = "",
+                note = "",
+                password = "",
+                limit = null
+            )
+        }
+    }
+}
