3f7db02214e3cf058e751fbf8e2b3c62161f6cd3
alperozturk96 <alper_ozturk@proton.me>
2026-05-05 16:16:59 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

---
+++ ui/src/main/java/com/nextcloud/android/common/ui/network/api/ApiMethod.kt
@@ -0,0 +1,15 @@
+/*
+ * Nextcloud Android Common Library
+ *
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.nextcloud.android.common.ui.network.api
+
+enum class ApiMethod(val type: String) {
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    DELETE("DELETE")
+}
