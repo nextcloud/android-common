9c0bc50e837fcf12d05bb3a379ce826de9d8ed0e
alperozturk96 <alper_ozturk@proton.me>
2026-06-01 10:29:27 +0300

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- ui/src/main/java/com/nextcloud/android/common/ui/share/component/DiscardDraftShareDialog.kt
+++ ui/src/main/java/com/nextcloud/android/common/ui/share/component/DiscardDraftShareDialog.kt
@@ -2,12 +2,13 @@
  * Nextcloud Android Common Library
  *
  * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: MIT
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 package com.nextcloud.android.common.ui.share.component
 
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -24,13 +25,13 @@ fun DiscardDraftShareDialog(
         title = { Text(stringResource(R.string.share_view_discard_draft_title)) },
         text = { Text(stringResource(R.string.share_view_discard_draft_message)) },
         confirmButton = {
-            TextButton(onClick = onDiscard) {
-                Text(stringResource(R.string.share_view_discard_draft_delete))
+            FilledTonalButton(onClick = onKeep) {
+                Text(stringResource(R.string.share_view_discard_draft_keep))
             }
         },
         dismissButton = {
-            TextButton(onClick = onKeep) {
-                Text(stringResource(R.string.share_view_discard_draft_keep))
+            TextButton(onClick = onDiscard) {
+                Text(stringResource(R.string.share_view_discard_draft_delete))
             }
         }
     )
