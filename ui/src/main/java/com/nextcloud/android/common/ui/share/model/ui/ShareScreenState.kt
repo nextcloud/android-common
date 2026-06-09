fce4eb508e7e657ae27585d249915fc4d6f972f9
alperozturk96 <alper_ozturk@proton.me>
2026-06-09 11:31:37 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- ui/src/main/java/com/nextcloud/android/common/ui/share/model/ui/ShareScreenState.kt
+++ ui/src/main/java/com/nextcloud/android/common/ui/share/model/ui/ShareScreenState.kt
@@ -8,9 +8,12 @@
 package com.nextcloud.android.common.ui.share.model.ui
 
 import com.nextcloud.android.common.ui.share.model.api.share.Share
+import com.nextcloud.android.common.ui.share.model.api.state.ShareState
 
 sealed class ShareScreenState {
     data object Empty: ShareScreenState()
     data object Loading: ShareScreenState()
     data class Loaded(val shares: List<Share>): ShareScreenState()
 }
+
+fun List<Share>.filtered(): List<Share> = filter { it.shareState != ShareState.DRAFT }
