a23d7cbce8c09ef54f52b8327a705de1469f534d
alperozturk96 <alper_ozturk@proton.me>
2026-08-10 14:22:28 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

---
+++ ui/src/main/java/com/nextcloud/android/common/ui/share/avatar/ShareAvatarRepository.kt
@@ -0,0 +1,37 @@
+/*
+ * Nextcloud Android Common Library
+ *
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.nextcloud.android.common.ui.share.avatar
+
+import android.util.Log
+import com.nextcloud.android.common.ui.network.auth.ServerCredentials
+import com.nextcloud.android.common.ui.network.http.NextcloudHttpClient
+import com.nextcloud.android.common.ui.network.model.dataOrElse
+import com.nextcloud.android.common.ui.share.model.api.share.Share
+import com.nextcloud.android.common.ui.share.model.api.source.Source
+import com.nextcloud.android.common.ui.share.repository.ShareRemoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class ShareAvatarRepository(private val credentials: ServerCredentials) {
+    companion object {
+        private const val TAG = "ShareAvatarRepository"
+        private const val SHARES_PAGE_SIZE = 3
+    }
+
+    suspend fun fetchShareAvatars(sourceId: String): List<Share>? = withContext(Dispatchers.IO) {
+        val client = NextcloudHttpClient.create(credentials)
+        val shareRepository = ShareRemoteRepository(client)
+        val result = shareRepository.fetchShares(
+            filterSourceTypeValue = sourceId,
+            limit = SHARES_PAGE_SIZE,
+            filterSourceTypeClass = Source.NODE_SOURCE_CLASS,
+            lastShareID = null
+        )
+        return@withContext result.dataOrElse { Log.e(TAG, "failed to fetch avatars") }
+    }
+}
