74fced0b4c6c3e807c76191753d0cd75280a2d0e
alperozturk96 <alper_ozturk@proton.me>
2026-09-03 09:31:45 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- ui/src/main/java/com/nextcloud/android/common/ui/share/model/ui/ShareItemType.kt
+++ ui/src/main/java/com/nextcloud/android/common/ui/share/model/ui/ShareItemType.kt
@@ -11,25 +11,40 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 
+private val OUTER_CORNER_RADIUS = 12.dp
+private val INNER_CORNER_RADIUS = 4.dp
+
 enum class ShareItemType {
-    Top, Mid, Bottom;
+    Single,
+    Top,
+    Mid,
+    Bottom;
 
     @Composable
-    fun getShape(): RoundedCornerShape {
-        return when (this) {
-            Top -> RoundedCornerShape(12.dp, 12.dp, 4.dp, 4.dp)
-            Mid -> RoundedCornerShape(4.dp, 4.dp, 4.dp, 4.dp)
-            Bottom -> RoundedCornerShape(4.dp, 4.dp, 12.dp, 12.dp)
-        }
+    fun getShape(): RoundedCornerShape = when (this) {
+        Single -> RoundedCornerShape(OUTER_CORNER_RADIUS)
+        Top -> RoundedCornerShape(
+            OUTER_CORNER_RADIUS,
+            OUTER_CORNER_RADIUS,
+            INNER_CORNER_RADIUS,
+            INNER_CORNER_RADIUS
+        )
+
+        Mid -> RoundedCornerShape(INNER_CORNER_RADIUS)
+        Bottom -> RoundedCornerShape(
+            INNER_CORNER_RADIUS,
+            INNER_CORNER_RADIUS,
+            OUTER_CORNER_RADIUS,
+            OUTER_CORNER_RADIUS
+        )
     }
 
     companion object {
-        fun type(index: Int, lastIndex: Int): ShareItemType {
-            return when (index) {
-                0 -> Top
-                lastIndex -> Bottom
-                else -> Mid
-            }
+        fun type(index: Int, lastIndex: Int): ShareItemType = when {
+            lastIndex == 0 -> Single
+            index == 0 -> Top
+            index == lastIndex -> Bottom
+            else -> Mid
         }
     }
 }
